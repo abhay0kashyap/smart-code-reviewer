9949adbd453cd4891795f72273b1e5349dc060f1
abhay0kashyap <131806283+abhay0kashyap@users.noreply.github.com>
2026-02-19 08:24:26 +0530

    feat(ui): add basic web interface for code analysis

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from analyzer.engine import analyze_code
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Smart Code Reviewer Running 🚀"
+    return render_template("index.html")
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
